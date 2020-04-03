a6b7845eb166d1a6ef5780c72626eefa9fb9d365
Drama Queen <dramaqueen@macintoshs-MacBook-Air.local>
2020-04-03 18:12:57 +0100

    created a modal with vue

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <button type="button" class="btn" @click="showModal">Open Modal!</button>
+    <modal v-show="isModalVisible" @close="closeModal"></modal>
+  </div>
+  
+</template>
+
+<script>
+
+import modal from './components/modal';
+
+export default {
+  name: 'app',
+  components:{
+    modal:modal
+  },
+  data () {
+    return {
+    isModalVisible:false
+    }
+  },
+  methods:{
+    showModal(){
+      this.isModalVisible = true;
+    },
+    closeModal(){
+      this.isModalVisible = false;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
